48687f700a940b2283e5c71c050a95f6bada9d41
MD JASHIMUDDIN <jash7818@gmail.com>
2022-01-15 20:40:01 +0530

    commit

--- codewithjash/urls.py
+++ codewithjash/urls.py
@@ -11,3 +11,7 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+
+urlpatterns += static(MEDIA_URL, document_root= MEDIA_ROOT)
+urlpatterns += static(STATIC_URL, document_root=settings.STATIC_ROOT)
